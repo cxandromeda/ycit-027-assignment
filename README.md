4daf82630a939bd457ffafcaafb4ddcc7e11f349
cxandromeda <cxandromeda@gmail.com>
2022-12-03 15:40:29 -0500

    meet all the requirements

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# YCIT-027 Assignment 1 of 1
+# YCIT-027 Assignment 1 of 1 .This is xi chen's homework
 
 ## Tasks
 
@@ -19,3 +19,5 @@
 **NOTE**: If you do not push your changes to your repository, you will not get the 10%
 
 **PROBLEMS?**: Please write below this line if you are encountering any problems. If you detail your problems here, you will get the 10%. Just be sure to push your changes to your forked repository.
+
+After two hours' work, I have fulfilled all the requirements.I mean the task is not that hard,but for the  part 5 I really waste two much time. firstly, I tried all the ways to get the element (<div class=".image-container">).But I failed again and again.It really makes me crazy. Finally, I changed the class name (class = "image") and my code works.
--- script.js
+++ script.js
@@ -1,11 +1,42 @@
+// This is xi chen's homework
+
+
 function getCatImage() {
-    fetch("https://api.thecatapi.com/v1/images/search")
-        .then((response) => {
+    return fetch("https://api.thecatapi.com/v1/images/search")
+        .then( (response) => {
             // Resolve the promise we got from calling then() to the promise that we get from calling json()
-            response.json();
+            return response.json();
+            
         })
         .then((data) => {
             console.log(data[0].url);
-            document.getElementById("catImage").src = data[0].url;
+            let divimg = document.querySelector('.image')
+            let imgc = document.createElement("img");
+            imgc.src = data[0].url;
+            imgc.id = "catImage"
+            divimg.appendChild(imgc);
+            let br = document.createElement("br")
+            divimg.appendChild(br);
+
+
+           
+
+            // document.getElementById("catImage").src = data[0].url
+
+
         });
 }
+
+
+// function getCatImage() {
+//     fetch("https://api.thecatapi.com/v1/images/search")
+//         .then((response) => {
+//             // Resolve the promise we got from calling then() to the promise that we get from calling json()
+//             response.json();
+            
+//         })
+//         .then((data) => {
+//             console.log(data[0].url);
+//             document.getElementById("catImage").src = data[0].url;
+//         });
+// }
